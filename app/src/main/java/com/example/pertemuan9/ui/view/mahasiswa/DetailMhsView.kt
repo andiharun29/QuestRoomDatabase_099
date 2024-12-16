0c91617398ec14e95e76957b46a175a90c984081
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-16 22:08:58 +0700

    itambahkan DetailMhsView untuk menampilkan detail mahasiswa dengan opsi edit dan hapus

--- app/src/main/java/com/example/pertemuan9/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pertemuan9/ui/view/mahasiswa/DetailMhsView.kt
@@ -6,15 +6,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -25,9 +31,59 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan9.Data.entity.Mahasiswa
+import com.example.pertemuan9.ui.CustomWidget.appBar
+import com.example.pertemuan9.ui.viewmodel.DetailMhsViewModel
 import com.example.pertemuan9.ui.viewmodel.DetailUiState
+import com.example.pertemuan9.ui.viewmodel.PenyediaViewModel
 import com.example.pertemuan9.ui.viewmodel.toMahasiswaEntity
+
+@Composable
+fun DetailMhsView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+
+){
+    Scaffold (
+        topBar = {
+            appBar(
+                judul = "Detail Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.nim)},
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteMhs()
+                onDeleteClick()
+            }
+        )
+    }
+}
+
 
 @Composable
 fun BodyDetailMhs(
@@ -77,7 +133,7 @@ fun BodyDetailMhs(
                 }
             }
         }
-        detailUiState.isUieventEmpty -> {
+        detailUiState.isUiEventEmpty -> {
             Box(
                 modifier = modifier.fillMaxWidth(),
                 contentAlignment = Alignment.Center
